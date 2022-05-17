c33123ac40056fe43ddbc7c87fa6dbfa66d9a780
Romanov7788 <romanov_sash@ukr.net>
2022-05-17 12:40:32 +0300

    commit

---
+++ src/components/Form.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function Form({ addTask }) {
+  const [userInput, setUserInput] = useState("");
+
+  const handleChange = (e) => {
+    setUserInput(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(userInput);
+    setUserInput("")
+  };
+  
+  const handleKeyPress = (e) => {
+      if(e.key ===  "Enter") {
+          handleSubmit(e)
+      }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={userInput}
+        type="text"
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+        placeholder="Введите значение..."
+      />
+      <button>Сохранить</button>
+    </form>
+  );
+}
+
+export default Form;
